2d7361ebe7af6421cba223e5e7aee08573333c21
Lui <6627294+yablochko8@users.noreply.github.com>
2025-04-14 21:43:00 +0100

    topnav

--- frontend/src/components/TopNav.tsx
+++ frontend/src/components/TopNav.tsx
@@ -2,7 +2,7 @@ import { ActionButtons } from "./ActionButtons";
 
 export const TopNav = () => {
     return (
-        <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar bg-base-100 shadow-sm fixed top-0 left-0 z-50">
             <div className="navbar-start">
                 <a className="btn btn-ghost text-xl text-slate-600">delicious.domains</a>
             </div>
