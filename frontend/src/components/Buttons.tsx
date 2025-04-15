6922bb72d8d30072442967f0bf273d54c16c886b
Lui <6627294+yablochko8@users.noreply.github.com>
2025-04-15 14:33:33 +0100

    undo on some icons

--- frontend/src/components/Buttons.tsx
+++ frontend/src/components/Buttons.tsx
@@ -59,15 +59,14 @@ export const ClearAllButton = () => {
     window.scrollTo(0, 0);
   };
 
+  const cta = "start again";
+
   return (
     <>
-      <button className="btn btn-outline hidden md:block" onClick={handleClick}>
-        <div className="flex flex-row w-full justify-between gap-2 items-center">
-          <div className="text-xl">{ActionIcons.startAgain}</div>
-          start again
-        </div>
+      <button className="btn btn-outline hidden md:block" onClick={handleClick} title={cta}>
+        {cta}
       </button>
-      <button className="btn btn-outline btn-square md:hidden text-2xl" onClick={handleClick}>
+      <button className="btn btn-outline btn-square md:hidden text-2xl" onClick={handleClick} title={cta}>
         {ActionIcons.startAgain}
       </button>
     </>
@@ -92,15 +91,13 @@ export const RefineInputsButton = () => {
     }
   };
 
+  const cta = "edit inputs";
   return (
     <>
-      <button className="btn btn-outline hidden md:block" onClick={handleClick}>
-        <div className="flex flex-row w-full justify-between gap-2 items-center">
-          <div className="text-xl">{ActionIcons.editInputs}</div>
-          edit inputs
-        </div>
+      <button className="btn btn-outline hidden md:block" onClick={handleClick} title={cta}>
+        {cta}
       </button>
-      <button className="btn btn-outline btn-square md:hidden text-2xl" onClick={handleClickMobile}>
+      <button className="btn btn-outline btn-square md:hidden text-2xl" onClick={handleClickMobile} title={cta}>
         {ActionIcons.editInputs}
       </button>
     </>
