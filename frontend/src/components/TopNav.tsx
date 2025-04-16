4c164d0bccf5a0ccfa315c46ac4bf7631b8ed949
Lui <6627294+yablochko8@users.noreply.github.com>
2025-04-16 13:08:19 +0100

    ubuntu font for h1 title

--- frontend/src/components/TopNav.tsx
+++ frontend/src/components/TopNav.tsx
@@ -6,9 +6,9 @@ export const TopNav = () => {
     return (
         <div className="navbar bg-base-100 shadow-sm fixed top-0 left-0 z-50">
             <div className="navbar max-w-7xl mx-auto w-full px-4">
-                <div className="navbar-start justify-start w-full">
-                    <img src="/logo.png" alt="dapper domains" className="w-10 h-10" />
-                    <a className="btn btn-ghost text-xl text-slate-600">{WEBSITE_NAME}</a>
+                <div className="navbar-start justify-start w-full cursor-pointer gap-2">
+                    <img src="/logo-light.png" alt="dapper domains" className="w-10 h-10" />
+                    <h1 className="text-primary">{WEBSITE_NAME}</h1>
                 </div>
                 <div className="navbar-end md:hidden">
                     <AboutButton />
